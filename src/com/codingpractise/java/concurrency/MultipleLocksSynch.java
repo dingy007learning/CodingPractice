08afdb208e7fb6aad569ec58713d688b79206b37
dingy007learning <dingy_007@yahoo.com>
2015-10-15 22:15:12 +0800

    Updating multiple locks tutorial

--- src/com/codingpractise/java/concurrency/MultipleLocksSynch.java
+++ src/com/codingpractise/java/concurrency/MultipleLocksSynch.java
@@ -7,10 +7,10 @@ public class MultipleLocksSynch {
 
 	Object lock1 = new Object();
 	Object lock2 = new Object();
-	
+
 	private static ArrayList<Integer> list1 = new ArrayList<Integer>();
 	private static ArrayList<Integer> list2 = new ArrayList<Integer>();
-	
+
 	Random rand = new Random();
 
 	private void nonSynchStageOne() {
@@ -30,14 +30,14 @@ public class MultipleLocksSynch {
 		}
 		list2.add(rand.nextInt(100));
 	}
-	
+
 	public void nonSynchProcesses() {
 		for (int i = 0; i<1000; i++) {
 			nonSynchStageOne();
 			nonSynchStageTwo();
 		}
 	}
-	
+
 	private synchronized void synchedStageOne() {
 		try {
 			Thread.sleep(1);
@@ -46,7 +46,7 @@ public class MultipleLocksSynch {
 		}
 		list1.add(rand.nextInt(100));
 	}
-	
+
 	private synchronized void synchedStageTwo() {
 		try {
 			Thread.sleep(1);
@@ -55,39 +55,43 @@ public class MultipleLocksSynch {
 		}
 		list2.add(rand.nextInt(100));
 	}
-	
+
 	public void synchedProcess() {
 		if(!list1.isEmpty()) list1.clear();
 		if(!list2.isEmpty()) list2.clear();
-		
+
 		for (int i=0; i<1000; i++) {
 			synchedStageOne();
 			synchedStageTwo();
 		}
 	}
-	
+
 	private void multiLockStageOne() {
-		try {
-			Thread.sleep(1);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		synchronized (lock1) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			list1.add(rand.nextInt(100));
 		}
-		list1.add(rand.nextInt(100));
 	}
-	
+
 	private void multiLockStageTwo() {
-		try {
-			Thread.sleep(1);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		synchronized(lock2) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			list2.add(rand.nextInt(100));
 		}
-		list2.add(rand.nextInt(100));
 	}
 
 	public void multiLockProcess() {
 		if(!list1.isEmpty()) list1.clear();
 		if(!list2.isEmpty()) list2.clear();
-		
+
 		for (int i = 0; i<1000; i++) {
 			multiLockStageOne();
 			multiLockStageTwo();
@@ -96,40 +100,40 @@ public class MultipleLocksSynch {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Starting the program...");
 		final MultipleLocksSynch multipleLocksSynch = new MultipleLocksSynch();
-		
+
 		long start = System.currentTimeMillis();
-		
+
 		Thread t1 = new Thread( new Runnable() {
 			@Override
 			public void run() {
 				multipleLocksSynch.nonSynchProcesses();
 			}
-			
+
 		});
-		
+
 		Thread t2 = new Thread( new Runnable() {
 			@Override
 			public void run() {
 				multipleLocksSynch.nonSynchProcesses();
 			}
-			
+
 		});
-		
-		
+
+
 		t1.start();
 		t2.start();
-		
+
 		t1.join();
 		t2.join();
-		
+
 		long end = System.currentTimeMillis();
-		
+
 		System.out.println("Time taken for the Non-Synchronized Processes = " + (end - start));
 		System.out.println("List1 size: " + list1.size());
 		System.out.println("List2 size: " + list2.size());
 		System.out.println("Two threads are working, notice that the Number of items in the list may not always be '2000', as is expected.");
 		System.out.println("The Process completes quickly, but may not be accurate.");
-		
+
 		start = System.currentTimeMillis();
 		t1 = new Thread(new Runnable() {
 			@Override
@@ -137,28 +141,28 @@ public class MultipleLocksSynch {
 				multipleLocksSynch.synchedProcess();
 			}
 		});
-		
+
 		t2 = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				multipleLocksSynch.synchedProcess();
 			}
 		});
-		
+
 		t1.start();
 		t2.start();
-		
+
 		t1.join();
 		t2.join();
-		
+
 		end = System.currentTimeMillis();
-		
+
 		System.out.println("Time taken for the Method level Synchronized Processes = " + (end - start));
 		System.out.println("List1 size: " + list1.size());
 		System.out.println("List2 size: " + list2.size());
 		System.out.println("The 'synchronized' keyword puts a Class level Lock, hence, when Thread 1 is locking StageOne method, Thread 2 cannot continue.");
 		System.out.println("The time taken is twice, but Process completed correctly.");
-		
+
 		start = System.currentTimeMillis();
 		t1 = new Thread(new Runnable() {
 			@Override
@@ -166,22 +170,22 @@ public class MultipleLocksSynch {
 				multipleLocksSynch.multiLockProcess();
 			}
 		});
-		
+
 		t2 = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				multipleLocksSynch.multiLockProcess();
 			}
 		});
-		
+
 		t1.start();
 		t2.start();
-		
+
 		t1.join();
 		t2.join();
-		
+
 		end = System.currentTimeMillis();
-		
+
 		System.out.println("Time taken for the Multi-Locked Synchronized-Block Processes = " + (end - start));
 		System.out.println("List1 size: " + list1.size());
 		System.out.println("List2 size: " + list2.size());
